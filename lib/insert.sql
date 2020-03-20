dbd3d100fe9cd197d3e4c7a75b91a900e2add40a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-20 08:48:24 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate",	5,	'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy",	3,	'F', 'Brown', 'Rambunctious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha",	4,	'F', 'Brown', 'Rambunctious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown",	8,	'F', 'Brown', 'Authoritarian', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa",	2,	'F', 'Brown', 'Rambunctious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch",	12,	'M', 'Brown', 'Aloof', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy",	3,	'F', 'Brown', 'Rambunctious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL,	10,	'F', 'Brown', 'Territorial', 1);
